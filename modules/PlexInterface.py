79f8f29e0f09bfd44dd530295b2162ab959ff059
Collin Heist <collinheist@gmail.com>
2022-02-01 13:52:48 -0700

    Removed functionality for inactive PlexInterface
    - Removed ability to created inactive PlexInterface object
    - Removed __active attribute in preparation for global preferences that enable/disable each interface

--- modules/PlexInterface.py
+++ modules/PlexInterface.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 from xml.etree.ElementTree import fromstring
 
 from requests import get, put
@@ -8,63 +7,45 @@ from modules.Show import Show
 
 class PlexInterface:
     """
-    This class describes a plex interface.
+    This class describes an interface to Plex for the purpose of pulling in new
+    title card images.
 
-    Get the plex token from: https://support.plex.tv/articles/204059436-finding-an-authentication-token-x-plex-token/
+    Get the plex token from this webpage:
+    https://support.plex.tv/articles/204059436-finding-an-authentication-token-x-plex-token/
     """
 
     def __init__(self, url: str, x_plex_token: str) -> None:
         """
-        Constructs a new instance.
+        Constructs a new instance of a Plex Interface.
         
-        :param      url:            The url
+        :param      url:            The url at which Plex is hosted.
 
-        :param      x_plex_token:   The x plex token
+        :param      x_plex_token:   The x plex token for sending API requests to
+                                    if the host device is untrusted.
         """
 
-        # If any args are None, this interface is inactive
-        if any(_ is None for _ in (url, x_plex_token)):
-            self.__active = False
-            self.base_url, self.base_params, self.library = None, None, None
-            return
-
-        self.__active = True
         url = url + ('' if url.endswith('/') else '/')
         self.base_url = url + 'library/'
         self.base_params = {'X-Plex-Token': x_plex_token}
 
-        # Structre is {library_name: {full_title: ratingKey, ...}, ...}
+        # Structure is {library_name: {full_title: ratingKey, ...}, ...}
         self.library = {}
         self.parse_plex_library()
-
-
-    def __bool__(self) -> bool:
-        """
-        Get the truthiness of this object.
-
-        :returns:   Whether this interface is active orn ot.
-        """
-
-        return self.__active
 
 
     def parse_plex_library(self) -> None:
         """
         Parse the associated plex library 
         """
-
-        if not self:
-            return
         
         url = self.base_url + 'sections'
         params = self.base_params
 
-        # Attempt to query Plex, if it errors, the URL was probably wrong - set inactive
+        # Attempt to query Plex, if it errors, the URL was probably wrong, error
         try:
             response = get(url=url, params=params)
         except Exception as e:
             error(f'Cannot query Plex. Returned error: "{e}"')
-            self.__active = False
             return None
 
         library_xml = fromstring(response.text)
@@ -97,29 +78,29 @@ class PlexInterface:
                 else:
                     full_title = f'{series_title} ({series_year})'
 
-                content_dict.update({Show.strip_specials(full_title): series_rating_key})
+                content_dict.update({
+                    Show.strip_specials(full_title): series_rating_key
+                })
 
             # Add this library to the object's dictionary
             self.library.update({library_title: content_dict})
 
-        info(f'Found {len(self.library)} Plex libraries ({", ".join(self.library.keys())})')
+        libraries = ", ".join(self.library.keys())
+        info(f'Found {len(self.library)} Plex libraries ({libraries})')
 
 
     def refresh_metadata(self, library: str, full_title: str) -> None:
         """
-        Refresh the given title's (if found in the specified library) metadata. This
-        effectively forces new title cards to be pulled in.
+        Refresh the given title's (if found in the specified library) metadata.
+        This effectively forces new title cards to be pulled in.
 
         If the library or full title is not found, no content is refreshed.
 
-        :param      library:    The name of the library where the content is. Must be
-                                a valid library name (i.e. matching Plex).
+        :param      library:    The name of the library where the content is.
+                                Must be a valid library name matching Plex.
         
-        :param      full_title: The full name (title, year) of the content to refresh.
+        :param      full_title: The full title of the content to refresh.
         """
-
-        if not self:
-            return
 
         # Match based on the stripped full title
         match_title = Show.strip_specials(full_title)
@@ -127,16 +108,16 @@ class PlexInterface:
         # Invalid library - error and exit
         if library not in self.library:
             error(f'Library "{library}" was not found in Plex')
-            return
+            return None
         
         # Valid library, invalid title - error and exit
         if match_title not in self.library[library]:
-            error(f'Series "{full_title}" was not found under library "{library}" in Plex')
-            return
+            error(f'Series "{full_title}" was not found under library '
+                  f'"{library}" in Plex')
+            return None
 
         # Get the plex ratingKey for this title, then PUT a metadata refresh
         rating_key = self.library[library][match_title]
-
         url = self.base_url + f'metadata/{rating_key}/refresh'
         put(url)
 
