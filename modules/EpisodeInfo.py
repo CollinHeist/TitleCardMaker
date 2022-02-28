b386fb2cd82dcacbca99e9f666671d2f428c0585
Collin Heist <collinheist@gmail.com>
2022-02-28 13:15:04 -0700

    Updated comments/docstring for EpisodeInfo class

--- modules/EpisodeInfo.py
+++ modules/EpisodeInfo.py
@@ -1,23 +1,25 @@
 class EpisodeInfo:
     """
-    This class describes static information about an Episode.
+    This class describes static information about an Episode, such as the
+    season, episode, and absolute number, as well as the various ID's associated
+    with it - such as Sonarr, TVDb, and TMDb.
     """
 
     def __init__(self, title: 'Title', season_number: int, episode_number: int,
                  abs_number: int=None) -> None:
         """
-        Constructs a new instance.
+        Constructs a new instance of an EpisdeInfo object.
         
-        :param      title:           The title
-        :param      season_number:   The season number
-        :param      episode_number:  The episode number
-        :param      abs_number:      The absolute number
+        :param      title:           The Title object for this entry.
+        :param      season_number:   The season number for this entry.
+        :param      episode_number:  The episode number for this entry.
+        :param      abs_number:      The absolute episode number for this entry.
         """
 
         # Store title
         self.title = title
 
-        # Store index
+        # Store indices
         self.season_number = int(season_number)
         self.season = self.season_number
 
@@ -100,19 +102,20 @@ class EpisodeInfo:
 
     def set_abs_number(self, abs_number: int) -> None:
         """
-        Sets the absolute number.
+        Set the absolute number for this object.
         
-        :param      abs_number:  The absolute number
+        :param      abs_number:  The absolute number to set.
         """
 
-        self.abs_number = abs_number
+        self.abs_number = int(abs_number)
+        self.abs = self.abs_number
 
 
     def set_sonarr_id(self, sonarr_id: int) -> None:
         """
-        Sets the sonarr identifier.
+        Sets the Sonarr ID for this object.
         
-        :param      sonarr_id:  The sonarr identifier
+        :param      tmdb_id:  The Sonarr ID to set.
         """
 
         self.sonarr_id = int(sonarr_id)
@@ -120,9 +123,9 @@ class EpisodeInfo:
 
     def set_tvdb_id(self, tvdb_id: int) -> None:
         """
-        Sets the tvdb identifier.
+        Sets the TVDb ID for this object.
         
-        :param      tvdb_id:  The tvdb identifier
+        :param      tmdb_id:  The TVDb ID to set.
         """
 
         self.tvdb_id = int(tvdb_id)
@@ -130,9 +133,9 @@ class EpisodeInfo:
 
     def set_tmdb_id(self, tmdb_id: int) -> None:
         """
-        Sets the tmdb identifier.
+        Sets the TMDb ID for this object.
         
-        :param      tmdb_id:  The tmdb identifier
+        :param      tmdb_id:  The TMDb ID to set.
         """
 
         self.tmdb_id = int(tmdb_id)
@@ -140,14 +143,14 @@ class EpisodeInfo:
 
     def copy_ids(self, other: 'EpisodeInfo') -> None:
         """
-        { function_description }
+        Copy all ID's from the given EpisodeInfo object. This copies the Sonarr,
+        TVDb, and TMDb ID's.
         
-        :param      other:  The other
+        :param      other:  The EpisodeInfo object to copy ID's from.
         """
 
         if not isinstance(other, EpisodeInfo):
-            raise TypeError(f"Can only copy ID's from another EpisodeInfo "
-                            f"object")
+            raise TypeError(f"Can only copy ID's from EpisodeInfo objects")
 
         self.sonarr_id = other.sonarr_id
         self.tvdb_id = other.tvdb_id
