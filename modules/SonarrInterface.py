11f4a2da48e90fcb5b13c949d8a6058b42bfaee2
Collin Heist <collinheist@gmail.com>
2022-02-27 20:23:16 -0700

    Rewrote SonarrInterface class
    - Removed ID and TVDB ID map code
    - Added runtime parsing of /series/ API endpoint instead of using a file of names/ids
    - Renamed _get_all_episode_data_for_id to __get_all_episode_info
    - Deleted unused get_absolute_episode_number method
    - Added __map_all_ids method that reads all series in Sonarr, and then creates dictionary of their full/alternate names to the Sonarr and TVDb ID's
    - Removed __map_id_to_tvdb and __set_sonarr_id method in favor of __set_ids method which adds both TVDb and Sonarr ID's to SeriesInfo objects

--- modules/SonarrInterface.py
+++ modules/SonarrInterface.py
@@ -1,6 +1,4 @@
 from datetime import datetime
-from pathlib import Path
-from pickle import dump, load, HIGHEST_PROTOCOL
 
 from modules.Debug import info, warn, error
 from modules.EpisodeInfo import EpisodeInfo
@@ -21,11 +19,6 @@ class SonarrInterface(WebInterface):
     """Datetime format string for airDateUtc field in Sonarr API requests"""
     __AIRDATE_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
 
-    """Path to the map of sonarr titles to ID's"""
-    __ID_MAP = Path(__file__).parent / '.objects' / 'sonarr_id_map.pkl'
-
-    """Path to the mapping of Sonarr IDs to TVDb ID's"""
-    __TVDB_ID_MAP = Path(__file__).parent / '.objects' / 'sid_tvdb_id.pkl'
 
     def __init__(self, url: str, api_key: str) -> None:
         """
@@ -38,22 +31,6 @@ class SonarrInterface(WebInterface):
         # Initialize parent WebInterface 
         super().__init__()
 
-        # Create objects directory if it does not exist
-        self.__ID_MAP.parent.mkdir(parents=True, exist_ok=True)
-        self.__TVDB_ID_MAP.parent.mkdir(parents=True, exist_ok=True)
-
-        # Attempt to read existing ID map
-        if self.__ID_MAP.exists():
-            with self.__ID_MAP.open('rb') as file_handle:
-                self.__id_map = load(file_handle)
-        else:
-            self.__id_map = {}
-
-        if self.__TVDB_ID_MAP.exists():
-            with self.__TVDB_ID_MAP.open('rb') as file_handle:
-                self.__tvdb_id_map = load(file_handle)
-        else:
-            self.__tvdb_id_map = {}
         # Add / if not given
         self.url = url + ('' if url.endswith('/') else '/')
 
@@ -62,123 +39,82 @@ class SonarrInterface(WebInterface):
             warn(f'Provided Sonarr URL ({self.url}) is not v3, add /v3/')
 
         # Base parameters for sending requests to Sonarr
-        self._param_base = {'apikey': api_key}
+        self.__api_key = api_key
+        self.__standard_params = {'apikey': api_key}
+
+        # Create blank dictionary of titles -> ID's
+        self.__series_ids = {}
+
+        # Parse all Sonarr series
+        self.__map_all_ids()
 
 
-    def __map_title_to_id(self, series_info: SeriesInfo) -> None:
+    def __set_ids(self, series_info: SeriesInfo) -> None:
         """
-        Add the given ID to the object's map. Write this updated map object to
-        the file.
+        Set the Sonarr series ID and the TVDb ID for the given SeriesInfo object
         
-        :param      series_info:    SeriesInfo to modify
+        :param      series_info:    SeriesInfo to modify.
         """
 
-        # Add to map
-        self.__id_map[series_info.full_name] = series_info.sonarr_id
-
-        # Write new map to file
-        with self.__ID_MAP.open('wb') as file_handle:
-            dump(self.__id_map, file_handle, HIGHEST_PROTOCOL)
-
-
-    def __map_id_to_tvdb(self, series_info: SeriesInfo) -> None:
-        """
-        Add the given Sonarr ID to the object's mapping of Sonarr ID's to TVDb
-        ID's. Write this updated map object to the file.
-        
-        :param      series_info:    SeriesInfo to modify
-        """
-
-        # Add to map
-        self.__tvdb_id_map[series_info.sonarr_id] = series_info.tvdb_id
-
-        # Write new map to file
-        with self.__TVDB_ID_MAP.open('wb') as file_handle:
-            dump(self.__tvdb_id_map, file_handle, HIGHEST_PROTOCOL)
-
-
-    def __set_sonarr_id(self, series_info: SeriesInfo) -> None:
-        """
-        Gets the series ID used by Sonarr to identify this show.
-        
-        :param      series_info:    SeriesInfo to modify
-        """
-
-        # If already mapped, return th
-        if series_info.full_name in self.__id_map:
-            series_info.set_sonarr_id(self.__id_map[series_info.full_name])
+        # Series does not exist in Sonarr
+        if series_info.full_match_name not in self.__series_ids:
             return None
+
+        # Get the Sonarr ID from the ID map
+        sonarr_id = self.__series_ids[series_info.full_match_name]['sonarr_id']
+        series_info.set_sonarr_id(sonarr_id)
+
+        # Get the TVDb ID from the ID map
+        tvdb_id = self.__series_ids[series_info.full_match_name]['tvdb_id']
+        series_info.set_tvdb_id(tvdb_id)
+
+
+    def __map_all_ids(self) -> None:
+        """
+        Map all Sonarr series to their Sonarr and TVDb ID's. This updates this
+        object's __series_ids attribute with keys of the full name for each
+        series (as well as the full name version of each alternate title) to a
+        dictionary with data 'sonarr_id' and 'tvdb_id'.
+        """
 
         # Construct GET arguments
-        url = f'{self._url_base}series/'
-        params = self._param_base
+        url = f'{self.url}series'
+        params = self.__standard_params
 
-        # Query Sonarr to get JSON of all series in the library
+        # Make GET request
         all_series = self._get(url, params)
 
-        # Go through each series
-        for show in all_series:
-            # Skip shows with a year mismatch, prevents parsing titles (slower)
-            if int(show['year']) != series_info.year:
-                continue
+        # Go through each series in Sonarr
+        for series in all_series:
+            # Get this series' internal (sonarr) and TVDb ID's
+            data = {
+                'sonarr_id': series['id'],
+                'tvdb_id': series['tvdbId'],
+            }
 
-            # Year matches, verify the given title matches main/alternate titles
-            if series_info.matches(show['title'], *show['alternateTitles']):
-                series_info.set_sonarr_id(show['id'])
-                self.__map_title_to_id(series_info)
+            # Map the primary title to these ID's
+            si = SeriesInfo(series['title'], series['year'])
+            self.__series_ids[si.full_match_name] = data
 
-                return None
-
-
-    def get_absolute_episode_number(self, series_info: SeriesInfo,
-                                    season_number: int,
-                                    episode_number: int) -> int:
-        """
-        Gets the absolute episode number of the given entry.
-        
-        :param      series_info:    SeriesInfo for the entry.
-        :param      season_number:  The season number of the entry.
-        :param      episode_number: The episode number of the entry.
-        
-        :returns:   The absolute episode number. None if not found, or if the
-                    entry does not have an absolute number.
-        """
-
-        # Get the ID for this series
-        self.__set_sonarr_id(series_info)
-
-        # If not found, skip
-        if not series_info.sonarr_id:
-            error(f'Cannot find series "{series_info}" in Sonarr')
-            return None
-
-        # Get all episodes, and match by season+episode number
-        for episode in self._get_all_episode_data_for_id(series_info):
-            season_match = (episode['season_number'] == season_number)
-            episode_match = (episode['episode_number'] == episode_number)
-
-            if season_match and episode_match and 'abs_number' in episode:
-                return episode['abs_number']
-
-        return None
+            # Map each alternate title to these ID's
+            for alt_title in series['alternateTitles']:
+                alt_si = SeriesInfo(alt_title['title'], series['year'])
+                self.__series_ids[alt_si.full_match_name] = data
 
 
     def list_all_series_id(self) -> None:
         """List all the series ID's of all shows used by Sonarr. """
 
         # Construct GET arguments
-        url = f'{self._url_base}series/'
-        params = self._param_base
+        url = f'{self.url}series/'
+        params = self.__standard_params
         
         # Query Sonarr to get JSON of all series in the library
         all_series = self._get(url, params)
 
-        if 'error' in all_series:
-            error(f'Sonarr returned error "{all_series["error"]}"')
-            return None
-
-        # Go through each series
+        # Go through each series in Sonarr
         for show in all_series:
+            # Print the main and alternate titles
             main_title = show['title']
             alt_titles = [_['title'] for _ in show['alternateTitles']]
 
@@ -187,27 +123,26 @@ class SonarrInterface(WebInterface):
             print(f'{show["id"]} : {titles}')
 
 
-    def _get_all_episode_data_for_id(self, series_id: int) -> list:
+    def __get_all_episode_info(self, series_id: int) -> [EpisodeInfo]:
         """
-        Gets all episode data for identifier. Only returns episodes that have
-        aired already.
+        Gets all episode info for the given series ID. Only returns episodes
+        that have aired already.
         
         :param      series_id:  The series identifier.
         
-        :returns:   All episode data for the given series id. Only entries that
-                    have ALREADY aired (or do not air) are returned.
+        :returns:   List of EpisodeInfo for the given series id. Only entries 
+                    that have ALREADY aired (or do not air) are returned.
         """
 
         # Construct GET arguments
-        url = f'{self._url_base}episode/'
-        params = self._param_base
-        params.update(seriesId=series_id)
+        url = f'{self.url}episode/'
+        params = {'apikey': self.__api_key, 'seriesId': series_id}
 
-        # Query Sonarr to get JSON of all episodes for this series id
+        # Query Sonarr to get JSON of all episodes for this series
         all_episodes = self._get(url, params)
+        all_episode_info = []
 
         # Go through each episode and get its season/episode number, and title
-        episode_info = []
         for episode in all_episodes:
             # Unaired episodes (such as specials) won't have airDateUtc key
             if 'airDateUtc' in episode:
@@ -219,48 +154,52 @@ class SonarrInterface(WebInterface):
                 if air_datetime > datetime.now():
                     continue
 
-            # Skip episodes whose titles aren't in Sonarr yet to avoid
-            # placeholder names
-            if episode['title'].lower() == 'tba':
+            # Skip episodes whose titles aren't in Sonarr to avoid placeholders
+            if episode['title'] in self.__PLACEHOLDER_NAMES:
                 continue
 
-            new_info = {
-                'season_number':    int(episode['seasonNumber']),
-                'episode_number':   int(episode['episodeNumber']),
-                'title':            Title(episode['title']),
-                # 'filename':         Path(episode['episodeFile']['path']).stem,
-            }
+            # Create EpisodeInfo object for this entry
+            # Non-canon episodes don't have absolute numbers
+            episode_info = EpisodeInfo(
+                Title(episode['title']),
+                episode['seasonNumber'],
+                episode['episodeNumber'],
+                episode.get('absoluteEpisodeNumber', None)
+            )
 
-            # Non-cannon episodes don't have an absolute number
-            if 'absoluteEpisodeNumber' in episode:
-                new_info['abs_number'] = int(episode['absoluteEpisodeNumber'])
+            # Add info for the Sonarr ID
+            episode_info.set_sonarr_id(episode['id'])
 
-            episode_info.append(new_info)
+            # If this episode has a non-zero TVDb ID (that exists), add that
+            if 'tvdbId' in episode and episode['tvdbId'] != 0:
+                episode_info.set_tvdb_id(episode['tvdbId'])
 
-        return episode_info
+            all_episode_info.append(episode_info)
+
+        return all_episode_info
 
 
-    def get_all_episodes_for_series(self, series_info: SeriesInfo) -> list:
+    def get_all_episodes_for_series(self,
+                                    series_info: SeriesInfo) -> [EpisodeInfo]:
         """
-        Gets all episode info for the given series title from Sonarr. The
-        returned info is season/episode number and title for each episode.
-
-        Only episodes that have already aired are returned.
+        Gets all episode info for the given series title from Sonarr. Only
+        episodes that have already aired are returned.
         
         :param      series_info:    SeriesInfo for the entry.
         
-        :returns:   List of dictionaries of episode data.
+        :returns:   List of EpisodeInfo objects for this series.
         """
 
         # Set the Sonarr ID for this series
-        self.__set_sonarr_id(series_info)
+        self.__set_ids(series_info)
 
         # If no ID was returned, error and return an empty list
         if series_info.sonarr_id == None:
             error(f'Series "{series_info}" not found in Sonarr')
+            breakpoint()
             return []
 
-        return self._get_all_episode_data_for_id(series_info.sonarr_id)
+        return self.__get_all_episode_info(series_info.sonarr_id)
 
 
     def set_tvdb_id_for_series(self, series_info: SeriesInfo) -> None:
@@ -271,27 +210,11 @@ class SonarrInterface(WebInterface):
         """
 
         # Set the Sonarr ID for this series
-        self.__set_sonarr_id(series_info)
+        self.__set_ids(series_info)
 
         # If no ID was returned, error and return
         if series_info.sonarr_id == None:
             error(f'Series "{series_info}" not found in Sonarr')
             return None
 
-        # If the series ID has already been mapped, return that value
-        if series_info.sonarr_id in self.__tvdb_id_map:
-            series_info.set_tvdb_id(self.__tvdb_id_map[series_info.sonarr_id])
-            return None
-
-        # Construct GET arguments
-        url = f'{self._url_base}series/{series_info.sonarr_id}'
-        params = self._param_base
-        params.update({'id': series_info.sonarr_id})
-
-        # Query Sonarr for info on this series
-        sonarr_info = self._get(url, params)
-
-        # Add this ID to the map
-        series_info.set_tvdb_id(sonarr_info['tvdbId'])
-        self.__map_id_to_tvdb(series_info)
         
