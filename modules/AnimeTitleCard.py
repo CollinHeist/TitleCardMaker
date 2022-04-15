8e58cfd073083ae1449a3e0adc59ccfc415385ef
Collin Heist <collinheist@gmail.com>
2022-04-15 17:23:32 -0600

    Fix for Anime filepaths with spaces

--- modules/AnimeTitleCard.py
+++ modules/AnimeTitleCard.py
@@ -317,7 +317,7 @@ class AnimeTitleCard(CardType):
 
         # Construct command for getting the width of the season text
         width_command = ' '.join([
-            f'convert -debug annotate {titled_image.resolve()}',
+            f'convert -debug annotate "{titled_image.resolve()}"',
             *season_text_command_list,
             ' null: 2>&1',
         ])
@@ -328,7 +328,7 @@ class AnimeTitleCard(CardType):
 
         # Construct command to add season and episode text
         command = ' '.join([
-            f'convert {titled_image.resolve()}',
+            f'convert "{titled_image.resolve()}"',
             *season_text_command_list,
             *self.__series_count_text_black_stroke(),
             f'-annotate +{75+width}+90 "{self.episode_text}"',
