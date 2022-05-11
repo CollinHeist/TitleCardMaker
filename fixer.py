5957ba670fbfa473804da86a38da0e8599446ec9
Collin Heist <collinheist@gmail.com>
2022-05-10 21:06:28 -0600

    Add blurring argument to fixer title card creation
    Added `--blur` argument to fixer manual title card creation to force pass blur argument (could have been done with arbitrary arguments, but hey..)

--- fixer.py
+++ fixer.py
@@ -42,42 +42,46 @@ title_card_group.add_argument(
     help='Create a title card with the given source image, written to the given'
          ' destination')
 title_card_group.add_argument(
+    '--blur',
+    action='store_true',
+    help='Blur the source image for this card')
+title_card_group.add_argument(
     '--episode',
     type=str,
     default='EPISODE',
     metavar='EPISODE_TEXT',
-    help="Specify this card's episode text")
+    help='The episode text for this card')
 title_card_group.add_argument(
     '--season',
     type=str,
     default=None,
     metavar='SEASON_TEXT',
-    help="Specify this card's season text")
+    help='The season text for this card')
 title_card_group.add_argument(
     '--title',
     type=str,
     nargs='+',
     default='',
     metavar=('TITLE_LINE'),
-    help="Specify this card's title text")
+    help="The title text for this card")
 title_card_group.add_argument(
     '--font', '--font-file',
     type=Path,
     default='__default',
     metavar='FONT_FILE',
-    help="Specify this card's custom font")
+    help="A custom font file for this card")
 title_card_group.add_argument(
     '--font-size', '--size',
     type=str,
     default='100%',
     metavar='SCALE%',
-    help='Specify a custom font scale (as percentage)')
+    help='A font scale (as percentage) for this card')
 title_card_group.add_argument(
     '--font-color', '--color',
     type=str, 
     default='__default',
     metavar='#HEX',
-    help='Specify a custom font color to use for this card')
+    help='A custom font color for this card')
 title_card_group.add_argument(
     '--vertical-shift', '--shift',
     type=float,
@@ -192,6 +196,7 @@ if hasattr(args, 'title_card'):
         font_size=float(args.font_size[:-1])/100.0,
         title_color=args.font_color,
         hide_season=(not bool(args.season)),
+        blur=args.blur,
         vertical_shift=args.vertical_shift,
         interline_spacing=args.interline_spacing,
         **arbitrary_data,
