3758758646c7bfae746d74ade204eeda3dc627ad
Collin Heist <collinheist@gmail.com>
2022-06-07 21:55:03 -0600

    Created basic season poster creator
    - Created SeasonPoster class
    - Part of #144

---
+++ modules/SeasonPoster.py
@@ -0,0 +1,92 @@
+from pathlib import Path
+
+from modules.Debug import log
+from modules.ImageMaker import ImageMaker
+
+class SeasonPoster(ImageMaker):
+    """
+    This class describes a type of ImageMaker that creates season posters.
+    Season posters take images, add a logo and season title.
+    """
+
+    """Directory where all reference files used by this card are stored"""
+    REF_DIRECTORY = Path(__file__).parent / 'ref' /'season_poster'
+
+    """Default font values for the season text"""
+    SEASON_TEXT_FONT = REF_DIRECTORY / 'Proxima Nova Semibold.otf'
+    SEASON_TEXT_COLOR = '#CFCFCF'
+
+    """Paths for the gradient overlay"""
+    GRADIENT_OVERLAY = REF_DIRECTORY / 'gradient.png'
+
+    __slots__ = ('source', 'destination', 'logo', 'season_text', 'font',
+                 'font_color', 'font_size', 'kerning')
+
+
+    def __init__(self, source: Path, destination: Path, logo: Path,
+                 season_text: str, font: Path=SEASON_TEXT_FONT,
+                 font_color: str=SEASON_TEXT_COLOR, font_size: float=1.0,
+                 kerning: float=1.0) -> None:
+        """
+        Constructs a new instance.
+        
+        :param      source:         The source file.
+        :param      destination:    The destination of this poster.
+        :param      logo:           The logo file.
+        :param      season_text:    The season text.
+        :param      font:           Font file for season text.
+        :param      font_color:     Font color for season text.
+        :param      font_size:      Scalar of season text.
+        :param      kerning:        Scalar of season text kerning.
+        """
+
+        # Initialize parent object
+        super().__init__()
+        
+        # Store provided file attributes
+        self.source = source
+        self.destination = destination
+        self.logo = logo
+
+        # Store text attributes
+        self.season_text = season_text
+
+        # Store customized font attributes
+        self.font = font
+        self.font_color = font_color
+        self.font_size = font_size
+        self.kerning = kerning
+
+
+    def create(self) -> None:
+        """Create the season poster defined by this object."""
+
+        # Get the scaled values for this poster
+        font_size = 40.0 * self.font_size
+        kerning = 15 * self.kerning
+
+        # Create the command
+        command = ' '.join([
+            f'convert',
+            f'"{self.source.resolve()}"',           # Resize input image
+            f'-gravity center',         
+            f'-resize "1000x1500^"',                    # Force into 1000x1500
+            f'"{self.GRADIENT_OVERLAY.resolve()}"', # Apply gradient
+            f'-compose Darken',                         # Darken mode
+            f'-composite',                              # Merge images
+            f'\( "{self.logo.resolve()}"',          # Add logo
+            f'-resize 730x \)',                         # Fit to 730px wide
+            f'-gravity south',                          # Begin merge placement
+            f'-compose Atop',       
+            f'-geometry +0+178',                        # Offset 178px from base
+            f'-composite',                              # Merge images
+            f'-font "{self.font.resolve()}"',       # Write season text
+            f'-fill "{self.font_color}"',
+            f'-pointsize {font_size}',
+            f'-kerning {kerning}',
+            f'-annotate +0+106 "{self.season_text}"',
+            f'"{self.destination.resolve()}"',
+        ])
+
+        self.image_magick.run(command)
+
