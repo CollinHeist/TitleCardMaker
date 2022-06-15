60ed5d286c1414d3e2f4217f149e7e8bcac0e92c
Collin Heist <collinheist@gmail.com>
2022-06-14 18:31:46 -0600

    Log source image downloadin
    Log source image gathering in Debug level

--- modules/Show.py
+++ modules/Show.py
@@ -670,6 +670,8 @@ class Show(YamlReader):
                 # If URL was returned by either interface, download
                 if image_url is not None:
                     WebInterface.download_image(image_url, episode.source)
+                    log.debug(f'Downloaded {episode.source.name} for {self} '
+                              f'from {source_interface.title()}')
                     break
 
         # Query TMDb for the backdrop if one does not exist and is needed
