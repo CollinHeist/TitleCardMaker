50c99e5371295bb5bc203699fee2d2d3ef0730a0
Collin Heist <collinheist@gmail.com>
2023-03-19 23:13:53 -0600

    Check-in HTML files
    - Define parts of the HTML files required for each major content endpoint
    - Part of #311

---
+++ app/templates/loadStaticContent.js
@@ -0,0 +1,72 @@
+// // Load sidebar
+// let xhr = new XMLHttpRequest();
+// xhr.open('GET', '/templates/sidebar.html', true);
+// xhr.onreadystatechange = function() {
+//   if (this.readyState !== 4) return;
+//   if (this.status !== 200) return;
+//   document.getElementById('nav-menu').innerHTML = this.responseText;
+// };
+// xhr.send();
+
+// // Load header
+// let xhr2 = new XMLHttpRequest();
+// xhr2.open('GET', '/templates/header.html', true);
+// xhr2.onreadystatechange = function() {
+//   if (this.readyState !== 4){return;}
+//   if (this.status !== 200) {return;}
+//   document.getElementById('page-header').innerHTML = this.responseText;
+// };
+// xhr2.send();
+
+// Initialize header search bar
+// $(document).ready(() => {
+//   $.fn.search.settings.templates = {
+//     standard: function(response) {
+//       let elements = response.results.map(({id, name, poster_url}) => {
+//         return `<a class="search result" href="/series/${id}"><div class="search content"><img src="${poster_url}" style="width: 35px; height: 50px; line-height: 50px">${name}</div></a>`;
+//       });
+//       return elements.join('');
+//     }
+//   }
+//   $('#search-bar').search({
+//     apiSettings: {
+//       url: '/api/series/search?query={query}',
+//     },
+//     // minCharacters: 3
+//   });
+// });
+
+// When the document is loaded...
+$(document).ready(function() {
+  // Load the header.html file
+  $.get('/templates/header.html')
+    .done(data => {
+      // Inject the header HTML into the page-header element
+      $('#page-header').html(data)
+      // Search bar uses the search API 
+      $('#search-bar').search({
+        apiSettings: {
+          url: '/api/series/search?query={query}&max_results=8',
+        }, 
+      });
+    })
+    .fail(($xhr, errorMsg) => $content.text(`Error: ${errorMsg}`));
+
+  // Load the sidebar.html file
+  $.get('/templates/sidebar.html')
+    .done(data => {
+      // Inject the sidebar HTML into the nev-menu element
+      $('#nav-menu').html(data)
+    })
+    .fail(($xhr, errorMsg) => $content.text(`Error: ${errorMsg}`));
+
+  // Search results should show a poster preview
+  $.fn.search.settings.templates = {
+    standard: function(response) {
+      let elements = response.results.map(({id, name, poster_url}) => {
+        return `<a class="search result" href="/series/${id}"><div class="search content"><img src="${poster_url}">${name}</div></a>`;
+      });
+      return elements.join('');
+    }
+  }
+})
