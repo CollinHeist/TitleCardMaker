0176b7401e8d51fd8c20457c69f2a60dfc2693a7
Collin Heist <collinheist@gmail.com>
2023-03-21 00:02:41 -0600

    Add EpisodeCount Statistic schema
    #311

--- app/schemas/statistic.py
+++ app/schemas/statistic.py
@@ -8,13 +8,18 @@ class Statistic(Base):
     unit: str
     description: str
 
-class CardCount(Statistic):
-    unit: str = 'Cards'
-    description: str = 'Number of managed title cards'
-
 class SeriesCount(Statistic):
     unit: str = 'Series'
     description: str = 'Number of series within TCM'
+
+class EpisodeCount(Statistic):
+    unit: str = 'Episodes'
+    description: str = 'Number of episodes'
+    value_text: str = '{value:,}'
+
+class CardCount(Statistic):
+    unit: str = 'Cards'
+    description: str = 'Number of managed title cards'
 
 class AssetSize(Statistic):
     description: str = 'Combined size of title cards created by TCM'
