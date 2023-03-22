b3639cb04e743e9c41b7f4d6ec563da40f633f20
Collin Heist <collinheist@gmail.com>
2023-03-21 23:53:39 -0600

    Add API endpoint to toggle an interface connection
    - Add /api/connection/{connection}/toggle endpoint to enable/disable connection to Emby, Jellyfin, Plex, Sonarr, and TMDb
    - Add Jellyfin to the SupportedConnection Literal
    - Part of #311

--- app/routers/api.py
+++ app/routers/api.py
@@ -3,18 +3,16 @@ from fastapi.responses import FileResponse
 
 from app.routers.connection import connection_router
 from app.routers.series import series_router
+from app.routers.statistics import statistics_router
+from app.routers.sync import sync_router
 
+# Create sub router for all API requests
 api_router = APIRouter(
     prefix='/api',
 )
 
-@api_router.post('/create-card')
-async def create_card(
-        source_file: UploadFile,
-        # TODO other params
-        ) -> FileResponse:
-
-    pass
-
+# Include sub-routers
 api_router.include_router(connection_router)
-api_router.include_router(series_router)
+api_router.include_router(font_router)
+api_router.include_router(settings_router)
+api_router.include_router(sync_router)
--- app/routers/connection.py
+++ app/routers/connection.py
@@ -1,27 +1,49 @@
-from typing import Literal
+from typing import Literal, Optional
+from urllib.parse import unquote
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Form, HTTPException, Query, Request
 from sqlalchemy.orm import Session
 
-from app.dependencies import get_database
+from app.dependencies import get_database, get_preferences, get_emby_interface, get_plex_interface, get_sonarr_interface, get_tmdb_interface
+from app.schemas.preferences import FilesizeUnit, Preferences, Style
+from modules.Debug import log
+from modules.EmbyInterface2 import EmbyInterface
+from modules.PlexInterface2 import PlexInterface
+from modules.SonarrInterface2 import SonarrInterface
+from modules.TMDbInterface2 import TMDbInterface
 
-SupportedConnection = Literal['emby', 'plex', 'sonarr', 'tmdb']
+SupportedConnection = Literal['emby', 'jellyfin', 'plex', 'sonarr', 'tmdb']
 
+# Create sub router for all /connection API requests
 connection_router = APIRouter(
     prefix='/connection',
-    tags=['connection', 'plex', 'emby', 'tmdb', 'sonarr'],
 )
 
 @connection_router.get('/{connection}')
-async def get_connection_details(
+def get_connection_details(
         connection: SupportedConnection,
-        db: Session = Depends(get_database)) -> dict:
-    
-    pass
+        db = Depends(get_database),
+        preferences = Depends(get_preferences)) -> Preferences:
 
-@connection_router.patch('/{connection}')
-async def update_connection(
+    ...
+
+
+@connection_router.put('/{connection}/{status}', status_code=204)
+def toggle_connection(
         connection: SupportedConnection,
-        db: Session = Depends(get_database)) -> dict:
-    
-    pass
+        status: Literal['enable', 'disable'],
+        preferences=Depends(get_preferences)) -> None:
+
+    if connection == 'emby':
+        preferences.use_emby = (status == 'enable')
+    elif connection == 'jellyfin':
+        preferences.use_jellyfin = (status == 'enable')
+    elif connection == 'plex':
+        preferences.use_plex = (status == 'enable')
+    elif connection == 'sonarr':
+        preferences.use_sonarr = (status == 'enable')
+    elif connection == 'tmdb':
+        preferences.use_tmdb = (status == 'enable')
+
+
+@connection_router.patch('/emby', status_code=200)
