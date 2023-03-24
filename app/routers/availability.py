99b594bbf16073b459dd061c1df993a2fecf025d
Collin Heist <collinheist@gmail.com>
2023-03-24 13:13:21 -0600

    Add basic /available API router
    Create /available API router + endpoints. Might change a lot of these later, but current endpoints are:
    - GET /available/card-types (not implemented)
    - GET /available/card-types/local
    - GET /available/card-types/remote (not implemented)
    - GET /available/episode-data-sources
    - GET /available/image-source-priority (don't like this)
    - GET /available/media-servers (redundant from preferences object?)
    - GET /available/libraries/{media_server}
    - GET /available/usernames/{emby or jellyfin}
    - GET /available/tags/sonarr
    - GET /available/fonts
    - GET /available/templates
    - GET /available/styles
    Part of #311.

---
+++ app/routers/availability.py
@@ -0,0 +1,174 @@
+from typing import Literal, Union
+
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app.dependencies import get_database, get_preferences, get_emby_interface,\
+    get_jellyfin_interface, get_plex_interface, get_sonarr_interface
+import app.models as models
+from app.schemas.card import CardType, LocalCardType, RemoteCardType
+from app.schemas.preferences import EpisodeDataSourceToggle, ImageSourceToggle,\
+    MediaServer, StyleOption
+from app.schemas.sonarr import Tag
+from modules.cards.available import LocalCards
+
+# Create sub router for all /connection API requests
+availablility_router = APIRouter(
+    prefix='/available',
+    tags=['Availibility'],
+)
+
+@availablility_router.get('/card-types',
+                          response_model=list[CardType])
+def get_available_card_types(
+        preferences=Depends(get_preferences)) -> list[CardType]:
+
+    ...
+     
+    return LocalCards
+
+
+@availablility_router.get('/card-types/local')
+def get_local_card_types(
+        preferences=Depends(get_preferences)) -> list[LocalCardType]:
+    
+    return LocalCards
+
+
+@availablility_router.get('/card-types/remote')
+def get_remote_card_types(
+        preferences=Depends(get_preferences)) -> list[RemoteCardType]:
+    
+    ...
+
+
+@availablility_router.get('/episode-data-sources')
+def get_available_episode_data_sources(
+        preferences=Depends(get_preferences)) -> list[EpisodeDataSourceToggle]:
+    
+    return [
+        {'name': source,
+         'value': source,
+         'selected': source == preferences.episode_data_source}
+        for source in preferences.valid_episode_data_sources
+    ]
+
+
+@availablility_router.get('/image-source-priority',
+                          response_model=list[ImageSourceToggle])
+def get_image_source_priority(
+        preferences=Depends(get_preferences)) -> list[EpisodeDataSourceToggle]:
+    
+    return [
+        {'name': source,
+         'value': source,
+         'selected': (source in preferences.image_source_priority)}
+        for source in (set(preferences.image_source_priority)
+                       | set(preferences.valid_image_sources))
+    ]
+
+
+@availablility_router.get('/media-servers')
+def get_available_media_servers(
+        preferences=Depends(get_preferences)) -> list[MediaServer]:
+    
+    return preferences.enabled_media_servers
+
+
+@availablility_router.get('/libraries/{media_server}')
+def get_server_libraries(
+        media_server: Literal['emby', 'jellyfin', 'plex'],
+        preferences = Depends(get_preferences),
+        emby_interface = Depends(get_emby_interface),
+        jellyfin_interface = Depends(get_jellyfin_interface),
+        plex_interface = Depends(get_plex_interface)) -> list[str]:
+    
+    if media_server == 'emby':
+        if preferences.use_emby:
+            return emby_interface.get_libraries()
+        return []
+    elif media_server == 'jellyfin':
+        if preferences.use_jellyfin:
+            return jellyfin_interface.get_libraries()
+        return []
+    elif media_server == 'plex':
+        if preferences.use_plex:
+            return plex_interface.get_libraries()
+        return []
+    
+    raise HTTPException(
+        status_code=400,
+        detail=f'Cannot get libraries for the "{media_server}" media server'
+    )
+
+
+@availablility_router.get('/usernames/emby', tags=['Emby'])
+def get_emby_usernames(
+        preferences=Depends(get_preferences),
+        emby_interface = Depends(get_emby_interface)) -> list[str]:
+
+    if preferences.use_emby and emby_interface:
+        return emby_interface.get_usernames()
+
+    return []
+
+
+@availablility_router.get('/usernames/jellyfin', tags=['Jellyfin'])
+def get_jellyfin_usernames(
+        preferences=Depends(get_preferences),
+        jellyfin_interface = Depends(get_jellyfin_interface)) -> list[str]:
+
+    if preferences.use_jellyfin and jellyfin_interface:
+        return jellyfin_interface.get_usernames()
+
+    return []
+
+
+@availablility_router.get('/tags/sonarr', tags=['Sonarr'])
+def get_sonarr_tags(
+        preferences=Depends(get_preferences),
+        sonarr_interface = Depends(get_sonarr_interface)) -> list[Tag]:
+    """
+    Get all tags defined in Sonarr.
+
+    Returns:
+        List of Tag objects. If Sonarr is disabled, then an empty list
+        is returned.
+    """
+
+    if preferences.use_sonarr and sonarr_interface:
+        return sonarr_interface.get_all_tags()
+
+    return []
+
+
+@availablility_router.get('/fonts', tags=['Fonts'])
+def get_available_fonts(db=Depends(get_database)) -> list[str]:
+
+    return [
+        font.name
+        for font in db.query(models.font.Font).all()
+    ]
+
+
+@availablility_router.get('/templates', tags=['Templates'])
+def get_available_templates(db=Depends(get_database)) -> list[str]:
+
+    return [
+        template.name
+        for template in db.query(models.template.Template).all()
+    ]
+
+
+@availablility_router.get('/styles')
+def get_available_styles() -> list[StyleOption]:
+    return [
+        {'name': 'Art', 'value': 'art', 'style_type': 'art'},
+        {'name': 'Blurred Art', 'value': 'art blur', 'style_type': 'art'},
+        {'name': 'Grayscale Art', 'value': 'art grayscale', 'style_type': 'art'},
+        {'name': 'Blurred Grayscale Art', 'value': 'art blur grayscale', 'style_type': 'art'},
+        {'name': 'Unique', 'value': 'unique', 'style_type': 'unique'},
+        {'name': 'Blurred Unique', 'value': 'blur unique', 'style_type': 'unique'},
+        {'name': 'Grayscale Unique', 'value': 'grayscale unique', 'style_type': 'unique'},
+        {'name': 'Blurred Grayscale Unique', 'value': 'blur grayscale unique', 'style_type': 'unique'},
+    ]
