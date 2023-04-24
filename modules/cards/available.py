ca0786e1011982a15660759da8f74c40f7edce5f
Collin Heist <collinheist@gmail.com>
2023-04-24 11:31:45 -0600

    Add Divider and Tinted Frame cards to web UI

--- modules/cards/available.py
+++ modules/cards/available.py
@@ -1,5 +1,6 @@
 from modules.cards.AnimeTitleCard import AnimeTitleCard
 from modules.cards.CutoutTitleCard import CutoutTitleCard
+from modules.cards.DividerTitleCard import DividerTitleCard
 from modules.cards.FadeTitleCard import FadeTitleCard
 from modules.cards.FrameTitleCard import FrameTitleCard
 from modules.cards.LandscapeTitleCard import LandscapeTitleCard
@@ -10,11 +11,13 @@ from modules.cards.RomanNumeralTitleCard import RomanNumeralTitleCard
 from modules.cards.StandardTitleCard import StandardTitleCard
 from modules.cards.StarWarsTitleCard import StarWarsTitleCard
 from modules.cards.TextlessTitleCard import TextlessTitleCard
+from modules.cards.TintedFrameTitleCard import TintedFrameTitleCard
 from modules.cards.TintedGlassTitleCard import TintedGlassTitleCard
 
 LocalCards = [
     AnimeTitleCard.API_DETAILS,
     CutoutTitleCard.API_DETAILS,
+    DividerTitleCard.API_DETAILS,
     FadeTitleCard.API_DETAILS,
     FrameTitleCard.API_DETAILS,
     LandscapeTitleCard.API_DETAILS,
@@ -25,5 +28,6 @@ LocalCards = [
     StandardTitleCard.API_DETAILS,
     StarWarsTitleCard.API_DETAILS,
     TextlessTitleCard.API_DETAILS,
-    TintedGlassTitleCard.API_DETAILS
+    TintedFrameTitleCard.API_DETAILS,
+    TintedGlassTitleCard.API_DETAILS,
 ]
