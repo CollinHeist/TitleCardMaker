66f911d94a02d23b2a4c1b693595aca8955e3f2d
Collin Heist <collinheist@gmail.com>
2023-05-08 18:25:46 -0600

    Add constraint regex to Sonarr ID model

--- app/schemas/ids.py
+++ app/schemas/ids.py
@@ -7,7 +7,7 @@ from app.schemas.base import Base, UNSPECIFIED
 EmbyID = Optional[PositiveInt]
 IMDbID =  Optional[constr(regex=r'^tt\d{4,}$')]
 JellyfinID = Optional[PositiveInt]
-SonarrID = Optional[str]
+SonarrID = Optional[constr(regex=r'^\d+-\d+$')]
 TMDbID = Optional[PositiveInt]
 TVDbID = Optional[PositiveInt]
 TVRageID = Optional[PositiveInt]
