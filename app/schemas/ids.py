9bdadfdba6b59b9ae1ee97cdd7a1ea4ed24e2205
Collin Heist <collinheist@gmail.com>
2023-06-02 12:51:26 -0600

    Change Jellyfin and Emby ID's from int to Hexstring

--- app/schemas/ids.py
+++ app/schemas/ids.py
@@ -2,9 +2,11 @@ from typing import Optional
 
 from pydantic import constr, PositiveInt
 
-EmbyID = Optional[PositiveInt]
+Hexstring = constr(regex=r'^[a-fA-F0-9]+$')
+
+EmbyID = Optional[Hexstring]
 IMDbID =  Optional[constr(regex=r'^tt\d{4,}$')]
-JellyfinID = Optional[PositiveInt]
+JellyfinID = Optional[Hexstring]
 SonarrID = Optional[constr(regex=r'^\d+-\d+$')]
 TMDbID = Optional[PositiveInt]
 TVDbID = Optional[PositiveInt]
