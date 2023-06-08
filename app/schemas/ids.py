deaf8148aa73d6d9bb49f4414b44a464a0bc8266
Collin Heist <collinheist@gmail.com>
2023-06-08 15:37:18 -0600

    Allow any integer for TMDb/TVDb/TVRage ID models
    Remove PositiveInt requirement

--- app/schemas/ids.py
+++ app/schemas/ids.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from pydantic import constr, PositiveInt
+from pydantic import constr
 
 Hexstring = constr(regex=r'^[a-fA-F0-9]+$')
 
@@ -8,6 +8,6 @@ EmbyID = Optional[Hexstring]
 IMDbID =  Optional[constr(regex=r'^tt\d{4,}$')]
 JellyfinID = Optional[Hexstring]
 SonarrID = Optional[constr(regex=r'^\d+-\d+$')]
-TMDbID = Optional[PositiveInt]
-TVDbID = Optional[PositiveInt]
-TVRageID = Optional[PositiveInt]
+TMDbID = Optional[int]
+TVDbID = Optional[int]
+TVRageID = Optional[int]
