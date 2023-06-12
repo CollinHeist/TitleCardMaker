ef4d9ec44b2308a9a54c937a57cdb2abe30f904a
Collin Heist <collinheist@gmail.com>
2023-06-12 15:33:54 -0600

    Refresh interface on get Dependency injection

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,10 +1,14 @@
 from typing import Generator
 
+from apscheduler.schedulers.background import BackgroundScheduler
+
 from app.database.session import (
     EmbyInterfaceLocal, ImageMagickInterfaceLocal, JellyfinInterfaceLocal,
     PreferencesLocal, PlexInterfaceLocal, Scheduler, SessionLocal,
     SonarrInterfaceLocal, TMDbInterfaceLocal
 )
+from app.models.preferences import Preferences
+
 from modules.EmbyInterface2 import EmbyInterface
 from modules.ImageMagickInterface import ImageMagickInterface
 from modules.JellyfinInterface2 import JellyfinInterface
@@ -12,6 +16,9 @@ from modules.PlexInterface2 import PlexInterface
 from modules.SonarrInterface2 import SonarrInterface
 from modules.TMDbInterface2 import TMDbInterface
 
+"""
+Miscellaneous global dependencies
+"""
 def get_database() -> Generator:
     db = SessionLocal()
     try:
@@ -19,23 +26,30 @@ def get_database() -> Generator:
     finally:
         db.close()
 
-def get_scheduler() -> 'BackgroundScheduler':
+def get_scheduler() -> BackgroundScheduler:
     return Scheduler
 
-def get_preferences() -> 'Preferences':
+def get_preferences() -> Preferences:
     return PreferencesLocal
 
-def get_emby_interface() -> EmbyInterface:
-    return EmbyInterfaceLocal
-
+"""
+Emby interface
+"""
 def refresh_emby_interface() -> None:
     preferences = get_preferences()
     global EmbyInterfaceLocal
     EmbyInterfaceLocal = EmbyInterface(**preferences.emby_arguments)
 
-def get_imagemagick_interface() -> ImageMagickInterface:
-    return ImageMagickInterfaceLocal
+def get_emby_interface() -> EmbyInterface:
+    preferences = get_preferences()
+    if preferences.use_emby and not EmbyInterfaceLocal:
+        refresh_emby_interface()
 
+    return EmbyInterfaceLocal
+
+"""
+ImageMagick interface
+"""
 def refresh_imagemagick_interface() -> None:
     preferences = get_preferences()
     global ImageMagickInterfaceLocal
@@ -43,34 +57,69 @@ def refresh_imagemagick_interface() -> None:
         **preferences.imagemagick_arguments
     )
 
-def get_jellyfin_interface() -> JellyfinInterface:
-    return JellyfinInterfaceLocal
+def get_imagemagick_interface() -> ImageMagickInterface:
+    return ImageMagickInterfaceLocal
+
+"""
+JellyFin interface
+"""
 
 def refresh_jellyfin_interface() -> JellyfinInterface:
     preferences = get_preferences()
     global JellyfinInterfaceLocal
     JellyfinInterfaceLocal = JellyfinInterface(**preferences.jellyfin_arguments)
 
-def get_plex_interface() -> PlexInterface:
-    return PlexInterfaceLocal
+def get_jellyfin_interface() -> JellyfinInterface:
+    preferences = get_preferences()
+    if preferences.use_jellyfin and not JellyfinInterfaceLocal:
+        refresh_jellyfin_interface()
+
+    return JellyfinInterfaceLocal
+
+"""
+Plex interface
+"""
 
 def refresh_plex_interface() -> None:
     preferences = get_preferences()
     global PlexInterfaceLocal
     PlexInterfaceLocal = PlexInterface(**preferences.plex_arguments)
 
-def get_sonarr_interface() -> SonarrInterface:
-    return SonarrInterfaceLocal
+def get_plex_interface() -> PlexInterface:
+    preferences = get_preferences()
+    if preferences.use_plex and not PlexInterfaceLocal:
+        refresh_plex_interface()
+
+    return PlexInterfaceLocal
+
+"""
+Sonarr interface
+"""
 
 def refresh_sonarr_interface() -> None:
     preferences = get_preferences()
     global SonarrInterfaceLocal
     SonarrInterfaceLocal = SonarrInterface(**preferences.sonarr_arguments)
 
-def get_tmdb_interface() -> TMDbInterface:
-    return TMDbInterfaceLocal
+def get_sonarr_interface() -> SonarrInterface:
+    preferences = get_preferences()
+    if preferences.use_sonarr and not SonarrInterfaceLocal:
+        refresh_sonarr_interface()
+
+    return SonarrInterfaceLocal
+
+"""
+TMDb interface
+"""
 
 def refresh_tmdb_interface() -> None:
     preferences = get_preferences()
     global TMDbInterfaceLocal
     TMDbInterfaceLocal = TMDbInterface(**preferences.tmdb_arguments)
+
+def get_tmdb_interface() -> TMDbInterface:
+    preferences = get_preferences()
+    if preferences.use_tmdb and not TMDbInterfaceLocal:
+        refresh_tmdb_interface()
+
+    return TMDbInterfaceLocal
