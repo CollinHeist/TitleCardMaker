cb793a0750a1826121df3b035a9c589bbcfdae8e
Collin Heist <collinheist@gmail.com>
2023-06-26 09:28:01 -0600

    Use more versatile pagination function

--- app/templates/helper.js
+++ app/templates/helper.js
@@ -1,3 +1,7 @@
+function sortStandardizeStr(text) {
+  return text.toString().replace(/^(a|an|the)(\s)/i, '');
+}
+
 function formatBytes(bytes, decimals = 2) {
   if (!+bytes) return '0 B'
 
@@ -42,4 +46,83 @@ function formatValidationError(response) {
   let message = '';
   detail.forEach(obj => message += `<li>${obj.loc[1]} : ${obj.msg}</li>`)
   return `<ul class="ui list">${message}</ul>`;
+}
+
+function createPageElement(pageNumber, pageText, active = false, navigateFunction) {
+  let element = document.createElement('a');
+  element.className = active ? 'active disabled item' : 'item';
+  element.innerText = pageText;
+  if (pageNumber !== undefined) { element.onclick = () => navigateFunction(pageNumber); }
+  return element;
+}
+
+function updatePagination(args) {
+  const {
+    paginationElementId = () => {},
+    navigateFunction = () => {},
+    page = 1,
+    pages = 1,
+    amountVisible = 5,
+    hideIfSinglePage = false,
+  } = args;
+
+  // Remove existing pagination links
+  $(`#${paginationElementId} a`).remove();
+
+  // Add no links if only one page
+  if (pages === 1) {
+    if (hideIfSinglePage) {
+      $(`#${paginationElementId}`).remove();
+    } else {
+      $(`#${paginationElementId}`).append([
+        createPageElement(1, 1, true, navigateFunction)
+      ]);
+    }
+    return;
+  }
+
+  // Determine how many links to show on the left and right of the active page
+  let links = [],
+      leftLinksBounds = Math.min(page-1, (amountVisible - 1) / 2),
+      rightLinksBounds = Math.min(pages-page, (amountVisible - 1) / 2);
+  let leftLinks = Math.floor(Math.min(page-1, leftLinksBounds + ((amountVisible-1) / 2) - rightLinksBounds)),
+      rightLinks = Math.floor(Math.min(pages, rightLinksBounds + ((amountVisible-1) / 2) - leftLinksBounds));
+
+  // Create previous pages
+  for (let pageNum = page - leftLinks; pageNum < page; pageNum++) {
+    // Skip invalid pages
+    if (pageNum <= 0) { continue; }
+    // First nav
+    if (pageNum > 1 && links.length === 0) {
+      links.push(createPageElement(1, '«', false, navigateFunction));
+    } else {
+      links.push(createPageElement(pageNum, pageNum, false, navigateFunction));
+    }
+  }
+  // Add active page
+  links.push(createPageElement(page, page, true, navigateFunction));
+  // Add next pages
+  for (let pageNum = page + 1; pageNum <= page + rightLinks && pageNum <= pages; pageNum++) {
+    // Last nav
+    if (pageNum !== pages && pageNum === page + rightLinks) {
+      links.push(createPageElement(pages, '»', false, navigateFunction));
+    } else {
+      links.push(createPageElement(pageNum, pageNum, false, navigateFunction));
+    }
+  }
+
+  $(`#${paginationElementId}`).append(links);
+}
+
+function initializeNullableBoolean(args) {
+  const {dropdownElement, value} = args;
+  const isTrue = (value === true) || (value === 'True');
+  const isFalse = (value === false) || (value === 'False');
+  dropdownElement.dropdown({
+    placeholder: 'Default',
+    values: [
+      {name: 'True', value: 'True', selected: isTrue},
+      {name: 'False', value: 'False', selected: isFalse},
+    ],
+  });
 }
