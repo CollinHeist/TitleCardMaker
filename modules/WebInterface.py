eadc8672c15427254960b016ca3187d6014644b6
Collin Heist <collinheist@gmail.com>
2023-07-28 17:34:58 -0600

    Raise ValueError instead of Exception

--- modules/WebInterface.py
+++ modules/WebInterface.py
@@ -176,9 +176,9 @@ class WebInterface:
             # Get content from URL
             image = get(image, timeout=30).content
             if len(image) == 0:
-                raise Exception(f'URL {image} returned no content error')
+                raise ValueError(f'URL {image} returned no content error')
             if any(bad_content in image for bad_content in WebInterface.BAD_CONTENT):
-                raise Exception(f'URL {image} returned (bad) malormed content')
+                raise ValueError(f'URL {image} returned (bad) malormed content')
 
             # Write content to file, return success
             destination.write_bytes(image)
