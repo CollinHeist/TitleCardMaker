f96824560e4bfa2015c51a02e01f144a2f7b252e
Collin Heist <collinheist@gmail.com>
2023-08-20 21:47:32 -0600

    Add docs to some JS functions

--- app/templates/js/theme.js
+++ app/templates/js/theme.js
@@ -1,3 +1,8 @@
+/*
+ * Refresh the theme of all HTML elements on the page. This sets the inverted
+ * CSS class of all elements under the main-content ID; as well as modals.
+ * Uninvertible conntent is excluded.
+ */
 function refreshTheme() {
   const siteTheme = window.localStorage.getItem('site-theme') || 'dark';
   const inverted = (siteTheme === 'dark');
@@ -5,9 +10,14 @@ function refreshTheme() {
   $('.modal:not(.basic):not(.uninvertible), .modal:not(.basic):not(.uninvertible) >* .ui:not(.uninvertible), .accordion:not(.uninvertible), .accordion >* .ui:not(.uninvertible)').toggleClass('inverted', inverted);
 }
 
+/*
+ * Toggle the current theme from light -> dark or dark -> light. This changes 
+ * the theme icon, updates the background image CSS property, updates the
+ * theme local storage variable, and refreshes all HTML.
+ */
 function toggleTheme() {
   const currentIcons = $('#theme-toggle i');
-  if (currentIcons.length === 0 ) { return; }
+  if (currentIcons.length === 0) { return; }
   // Toggle inverted class modifier, update button sun <-> moon, and set background color
   // Light -> Dark
   if (currentIcons[0].classList.contains('sun')) {
