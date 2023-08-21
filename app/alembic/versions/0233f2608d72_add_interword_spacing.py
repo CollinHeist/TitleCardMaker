460d1fc80fbfe7b8b0eea248f0bb935b98b8d8f7
Collin Heist <collinheist@gmail.com>
2023-08-20 22:03:53 -0600

    Initialize Card interword spacing Column with 0

--- app/alembic/versions/0233f2608d72_add_interword_spacing.py
+++ app/alembic/versions/0233f2608d72_add_interword_spacing.py
@@ -46,7 +46,12 @@ def upgrade() -> None:
         batch_op.add_column(sa.Column('font_interword_spacing', sa.Integer(), nullable=True))
 
     with op.batch_alter_table('card', schema=None) as batch_op:
-        batch_op.add_column(sa.Column('font_interword_spacing', sa.Integer(), nullable=True))
+        batch_op.add_column(sa.Column(
+            'font_interword_spacing',
+            sa.Integer(),
+            nullable=True,
+            server_default=str(0),
+        ))
 
 
 def downgrade() -> None:
