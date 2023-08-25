e0b0d09046d9acc98f95ed8dec7ee5abfe2c4d42
CollinHeist <collinheist@gmail.com>
2023-08-25 15:51:20 -0600

    Update database ID schemas for multi ID strings
    database ID's are now formatted as {interface_id}:{database_id},{...} - e.g. 0:abcdef,1:def123

--- app/schemas/ids.py
+++ app/schemas/ids.py
@@ -2,12 +2,12 @@ from typing import Optional
 
 from pydantic import constr
 
-Hexstring = constr(regex=r'^[a-fA-F0-9]+$')
 
-EmbyID = Optional[Hexstring]
+Hexstring = constr(regex=r'^[a-fA-F0-9]+$')
+EmbyID = Optional[constr(regex=r'^(\d+[:-][a-fA-F0-9]+,)*\d+[:-][a-fA-F0-9]+$')]
 IMDbID =  Optional[constr(regex=r'^tt\d{4,}$')]
 JellyfinID = Optional[Hexstring]
-SonarrID = Optional[constr(regex=r'^\d+-\d+$')]
+SonarrID = Optional[constr(regex=r'^(\d+[:-]\d+,)*\d+[:-]\d+$')]
 TMDbID = Optional[int]
 TVDbID = Optional[int]
 TVRageID = Optional[int]
