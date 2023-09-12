c5d383185b815fecf8925a73f8ea7aecdf6d4eaa
Collin Heist <collinheist@gmail.com>
2023-09-11 21:38:56 -0600

    Simplify directory structures
    On Docker, only entry volume is /config. Non-Docker, local ./config/ directory is used. /config/assets, /config/backups, /config/card_types, /config/cards/, /config/logs, and /config/source

--- app/database/session.py
+++ app/database/session.py
@@ -29,9 +29,9 @@ from modules.TMDbInterface2 import TMDbInterface
 IS_DOCKER = environ.get('TCM_IS_DOCKER', 'false').lower() == 'true'
 
 # Get URL of the SQL Database - based on whether in Docker or not
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./db.sqlite'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./config/db.sqlite'
 if IS_DOCKER:
-    SQLALCHEMY_DATABASE_URL = 'sqlite:////config/source/db.sqlite'
+    SQLALCHEMY_DATABASE_URL = 'sqlite:////config/db.sqlite'
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}
 )
@@ -49,25 +49,26 @@ def backup_database(*, log: Logger = log) -> Path:
     """
 
     # Determine file to back up database to
-    now = datetime.now().strftime('%Y.%m.%d_%H.%M.%S')
+    BACKUP_DT_FORMAT = '%Y.%m.%d_%H.%M.%S'
+    now = datetime.now().strftime(BACKUP_DT_FORMAT)
     if IS_DOCKER:
-        database = Path('/config/source/db.sqlite')
+        database = Path('/config/db.sqlite')
         backup_file = Path(f'/config/backups/db.sqlite.{now}')
     else:
-        database = Path('./db.sqlite')
+        database = Path('./config/db.sqlite')
         backup_file = Path(f'./backups/db.sqlite.{now}')
 
     # Remove databases older than 4 weeks
     for prior_backup in backup_file.parent.glob('db.sqlite.*'):
         try:
             date = datetime.strptime(
-                prior_backup.name, 'db.sqlite.%Y.%m.%d_%H.%M.%S'
+                prior_backup.name, f'db.sqlite.{BACKUP_DT_FORMAT}'
             )
         except ValueError:
-            log.warning(f'Cannot identify date of backup "{prior_backup}"')
+            log.warning(f'Cannot identify date of backup file "{prior_backup}"')
             continue
 
-        if date < datetime.now() - timedelta(days=28):
+        if date < datetime.now() - timedelta(weeks=4):
             prior_backup.unlink(missing_ok=True)
             log.debug(f'Deleted old database backup file "{prior_backup}"')
 
@@ -89,13 +90,16 @@ def regex_replace(pattern, replacement, string):
     return re_sub(pattern, replacement, string)
 
 @listens_for(engine, 'connect')
-def register_custom_functions(dbapi_connection, connection_record): # pylint: disable=unused-argument
+def register_custom_functions(
+        dbapi_connection,
+        connection_record, # pylint: disable=unused-argument
+    ) -> None:
     """When the engine is connected, register the `regex_replace` function"""
     dbapi_connection.create_function('regex_replace', 3, regex_replace)
 
 
 SessionLocal = sessionmaker(
-    bind=engine, expire_on_commit=False, autocommit=False, autoflush=False, 
+    bind=engine, expire_on_commit=False, autocommit=False, autoflush=False,
 )
 Base = declarative_base()
 
@@ -108,11 +112,11 @@ Scheduler = BackgroundScheduler(
     misfire_grace_time=600,
 )
 
-# Preference file/object
-preferences_file = Path(__file__).parent.parent.parent / 'modules' / '.objects'\
-    / 'prefs.json'
+# Config file/object
+preferences_file =\
+    Path(__file__).parent.parent.parent / 'config' / 'config.pickle'
 if IS_DOCKER:
-    preferences_file = Path('/config/source/prefs.json')
+    preferences_file = Path('/config/config.pickle')
 PreferencesLocal = Preferences(preferences_file)
 
 
