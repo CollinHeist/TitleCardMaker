c649128401ba5d859cb4f9fc0d659ef26067c557
CollinHeist <collinheist@gmail.com>
2023-09-14 14:59:56 -0600

    Update schedule.py

--- app/schemas/schedule.py
+++ app/schemas/schedule.py
@@ -10,31 +10,15 @@ from app.schemas.base import Base
 Base classes
 """
 class NewJob(Base):
-    id: str = Field(description='Unique ID of the Job')
-    function: Callable[..., None] = Field(
-        description='Function this Job will run',
-    )
-    seconds: PositiveInt = Field(
-        description='How often (in seconds) to run this Job'
-    )
+    id: str
+    function: Callable[..., None]
+    seconds: PositiveInt
     crontab: str
-    description: str = Field(description='Description of the Job')
-    internal: bool = Field(
-        default=False,
-        description='Whether this Job is internal',
-    )
-    running: bool = Field(
-        default=False,
-        description='Whether this Job is currently running'
-    )
-    previous_start_time: Optional[datetime] = Field(
-        default=None,
-        description='When this Job was last started',
-    )
-    previous_end_time: Optional[datetime] = Field(
-        default=None,
-        description="When this Job's previous execution last finished",
-    )
+    description: str
+    internal: bool = False
+    running: bool = False
+    previous_start_time: Optional[datetime] = None
+    previous_end_time: Optional[datetime] = None
 
 """
 Update classes
@@ -51,16 +35,10 @@ class UpdateSchedule(Base):
 Return classes
 """
 class ScheduledTask(Base):
-    id: str = Field(description='Unique ID of the Task')
-    description: str = Field(description='Description of the Task')
-    frequency: Optional[PositiveInt] = Field(
-        default=None,
-        description='How often (in seconds) the Task runs'
-    )
+    id: str
+    description: str
+    frequency: Optional[PositiveInt] = None
     crontab: Optional[str] = None
-    next_run: str = Field(description='Next runtime for the Task')
-    previous_duration: Optional[timedelta] = Field(
-        default=None,
-        description="How long this Job's previous execution took"
-    )
-    running: bool = Field(description='Whether this Task is currently running')
+    next_run: str
+    previous_duration: Optional[timedelta] = None
+    running: bool
