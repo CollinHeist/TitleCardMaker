06454cd08644952a5b29ac3892989af29825f1d8
Collin Heist <collinheist@gmail.com>
2023-09-21 16:02:27 -0600

    Update Connection Pydantic models

--- app/schemas/connection.py
+++ app/schemas/connection.py
@@ -1,17 +1,30 @@
 # pylint: disable=missing-class-docstring,missing-function-docstring,no-self-argument
-from typing import Literal, Optional
-from pydantic import ( # pylint: disable=no-name-in-module
-    AnyUrl, NonNegativeInt, PositiveInt, SecretStr, constr, validator  
-)
+from typing import Literal, Optional, Union
+from pydantic import AnyUrl, NonNegativeInt, SecretStr, constr, validator # pylint: disable=no-name-in-module
 
 from app.schemas.base import Base, MediaServer, UpdateBase, UNSPECIFIED
+from modules.EmbyInterface2 import EmbyInterface
+from modules.JellyfinInterface2 import JellyfinInterface
+from modules.PlexInterface2 import PlexInterface
+from modules.SonarrInterface2 import SonarrInterface
+from modules.TMDbInterface2 import TMDbInterface
 
+
+# Names of acceptable server types
+ServerName = Literal['Emby', 'Jellyfin', 'Plex', 'Sonarr']
+
+# Any acceptable Interface to an EpisodeDataSource
+EpisodeDataSourceInterface = Union[
+    EmbyInterface, JellyfinInterface, PlexInterface, SonarrInterface,
+    TMDbInterface
+]
 
 # Match hexstrings of A-F and 0-9.s
 Hexstring = constr(regex=r'^[a-fA-F0-9]+$')
 
 # Acceptable units for filesize limits
 FilesizeUnit = Literal['Bytes', 'Kilobytes', 'Megabytes']
+FilesizeLimit = constr(regex=r'\d+\s+(Bytes|Kilobytes|Megabytes)')
 
 # Accepted TMDb 2-letter language codes
 TMDbLanguageCode = Literal[
@@ -30,14 +43,17 @@ class SonarrLibrary(Base):
     interface_id: int
 
 class BaseServer(Base):
-    interface_id: int
+    id: int
+    interface: ServerName
     enabled: bool
+    name: str
     url: AnyUrl
     api_key: SecretStr
     use_ssl: bool = True
 
 class BaseNewConnection(Base):
     enabled: bool = True
+    name: str = 'Server'
 
 class BaseNewServer(BaseNewConnection):
     url: AnyUrl
@@ -45,30 +61,36 @@ class BaseNewServer(BaseNewConnection):
     use_ssl: bool = True
 
 class BaseNewMediaServer(BaseNewServer):
-    filesize_limit_number: PositiveInt = 5
-    filesize_limit_unit: FilesizeUnit = 'Megabytes'
+    filesize_limit: FilesizeLimit = '5 Megabytes'
 
 class BaseUpdateServer(UpdateBase):
     url: AnyUrl = UNSPECIFIED
     use_ssl: bool = UNSPECIFIED
 
 class BaseUpdateMediaServer(BaseUpdateServer):
-    filesize_limit_number: PositiveInt = UNSPECIFIED
-    filesize_limit_unit: FilesizeUnit = UNSPECIFIED
+    filesize_limit: FilesizeLimit = UNSPECIFIED
 
 """
 Creation classes
 """
 class NewEmbyConnection(BaseNewMediaServer):
+    name: str = 'Emby Server'
+    interface: ServerName = 'Emby'
     username: Optional[str] = None
 
 class NewJellyfinConnection(BaseNewMediaServer):
+    name: str = 'Jellyfin Server'
+    interface: ServerName = 'Jellyfin'
     username: Optional[str] = None
 
 class NewPlexConnection(BaseNewMediaServer):
+    name: str = 'Plex Server'
+    interface: ServerName = 'Plex'
     integrate_with_pmm: bool = False
 
 class NewSonarrConnection(BaseNewServer):
+    name: str = 'Sonarr Server'
+    interface: ServerName = 'Sonarr'
     downloaded_only: bool = True
     libraries: list[SonarrLibrary] = []
 
@@ -115,17 +137,23 @@ class UpdateTMDb(UpdateBase):
 """
 Return classes
 """
+class ServerConnection(BaseServer):
+    username: Optional[int]
+    filesize_limit: Optional[FilesizeLimit]
+    downloaded_only: bool
+    libraries: list[SonarrLibrary]
+
 class EmbyConnection(BaseServer):
     username: Optional[str]
-    # filesize_limit: Optional[int] # TODO evaluate
+    filesize_limit: FilesizeLimit
 
 class JellyfinConnection(BaseServer):
     username: Optional[str]
-    # filesize_limit: Optional[int] # TODO evaluate
+    filesize_limit: FilesizeLimit
 
 class PlexConnection(BaseServer):
     integrate_with_pmm: bool
-    # filesize_limit: Optional[int] # TODO evaluate
+    filesize_limit: FilesizeLimit
 
 class SonarrConnection(BaseServer):
     downloaded_only: bool
