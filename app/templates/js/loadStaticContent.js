20b6dad54fa316a8ed7bc74795d1e3a8f7e4ca2c
Collin Heist <collinheist@gmail.com>
2023-10-05 20:19:45 -0600

    Update loadStaticContent.js

--- app/templates/js/loadStaticContent.js
+++ app/templates/js/loadStaticContent.js
@@ -1,28 +1,4 @@
-// When the document is loaded...
 $(document).ready(function() {
-  // Search bar uses the search API 
-  $('#search-bar').search({
-    apiSettings: {
-      url: '/api/series/search?name={query}&size=8&page=1',
-      onResponse: serverResponse => { return serverResponse.items; },
-    },
-  });
-  // Load the header.html file
-  // $.get('/templates/header.html')
-  //   .done(data => {
-  //     // Inject the header HTML into the page-header element
-  //     $('#page-header').html(data);
-      
-  //     // Search bar uses the search API 
-  //     $('#search-bar').search({
-  //       apiSettings: {
-  //         url: '/api/series/search?name={query}&size=8&page=1',
-  //         onResponse: serverResponse => { return serverResponse.items; },
-  //       },
-  //     });
-  //   })
-  //   .fail(($xhr, errorMsg) => $content.text(`Error: ${errorMsg}`));
-
   // Trigger events on keypresses
   $(document).keypress((event) => {
     if (event.target.tagName !== 'INPUT' && event.target.tagName !== 'TEXTAREA' && !event.target.isContentEditable) {
@@ -32,14 +8,6 @@ $(document).ready(function() {
       else if (event.key === 'H' && event.shiftKey) { window.location.href = '/'; }
     }
   });
-
-  // Load the sidebar.html file
-  // $.get('/templates/sidebar.html')
-  //   .done(data => {
-  //     // Inject the sidebar HTML into the nev-menu element
-  //     $('#nav-menu').html(data);
-  //   })
-  //   .fail(($xhr, errorMsg) => $content.text(`Error: ${errorMsg}`));
 
   // Search results should show a poster preview
   $.fn.search.settings.templates = {
@@ -59,4 +27,12 @@ $(document).ready(function() {
       }
     },
   }
+
+  // Search bar uses the search API
+  $('#search-bar').search({
+    apiSettings: {
+      url: '/api/series/search?name={query}&size=8&page=1',
+      onResponse: serverResponse => { return serverResponse.items; },
+    },
+  });
 })
