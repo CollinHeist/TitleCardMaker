8f03ac6e6938467cfc63401c06f0b899502ce16b
Collin Heist <collinheist@gmail.com>
2023-10-10 15:25:09 -0600

    Adjust interface ID type regex

--- app/schemas/ids.py
+++ app/schemas/ids.py
@@ -4,10 +4,10 @@ from typing import Optional
 from pydantic import constr # pylint: disable=no-name-in-module
 
 
-EmbyID = Optional[constr(regex=r'^(\d+[:-][a-fA-F0-9]+,)*\d+[:-][a-fA-F0-9]+$')]
+EmbyID = constr(regex=r'^(\d+[:-][a-fA-F0-9]+,)*\d+[:-][a-fA-F0-9]+$|^$')
 IMDbID =  Optional[constr(regex=r'^tt\d{4,}$')]
-JellyfinID = Optional[constr(regex=r'^[a-fA-F0-9]+$')]
-SonarrID = Optional[constr(regex=r'^(\d+[:-]\d+,)*\d+[:-]\d+$')]
+JellyfinID = constr(regex=r'^[a-fA-F0-9]+$|^$')
+SonarrID = constr(regex=r'^(\d+[:-]\d+,)*\d+[:-]\d+$|^$')
 TMDbID = Optional[int]
 TVDbID = Optional[int]
 TVRageID = Optional[int]
