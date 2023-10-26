cf7637b4bb6090b762ef4ed8c3b1b9cc0d0cc8f9
Collin Heist <collinheist@gmail.com>
2023-10-25 22:23:07 -0600

    Use CSS color mix to redden elements when login fails

--- app/templates/js/login.js
+++ app/templates/js/login.js
@@ -2,22 +2,10 @@
  * Increase the red value of the background of the login element by
  * the given amount.
  */
-function makeRedder(increment=50) {
-  const loginDiv = document.getElementById('login');
-  const currentBackground = window.getComputedStyle(loginDiv).background;
-  const gradientMatch = currentBackground.match(/linear-gradient\((.*)\)/);
-  const colorsMatch = gradientMatch[1].match(/rgba\((\d+),\s*(\d+),\s*(\d+),\s*(\d*\.\d+)\)/g);
-  const rgbaColor = colorsMatch[1];
-  const rgbaValuesMatch = rgbaColor.match(/rgba\((\d+),\s*(\d+),\s*(\d+),\s*(\d*\.\d+)\)/);
-  const red = parseInt(rgbaValuesMatch[1], 10);
-  const green = parseInt(rgbaValuesMatch[2], 10);
-  const blue = parseInt(rgbaValuesMatch[3], 10);
-  const alpha = parseFloat(rgbaValuesMatch[4]);
-
-  // Modify the red value
-  const newRed = Math.min(Math.max(red + increment, 0), 255);
-  const modifiedColor = `rgba(${newRed}, ${green}, ${blue}, ${alpha})`;
-  loginDiv.style.background = currentBackground.replace(rgbaColor, modifiedColor);;
+let redMixin = 0;
+function makeRedder(increment=20) {
+  redMixin = Math.min(Math.max(redMixin + increment, 0), 100);
+  document.querySelector(':root').style.setProperty('--red-mixin', `${redMixin}%`);
 }
 
 /*
@@ -41,6 +29,9 @@ function submitForm() {
     contentType: false,
     processData: false,
     success: token => {
+      // Incrementally reset red mixin on success
+      setInterval(() => makeRedder(-10), 45);
+
       // Token expires in 1 hour
       const date = new Date();
       date.setTime(date.getTime() + (1000 * 60 * 60 * 24 * 2));
@@ -49,24 +40,22 @@ function submitForm() {
       document.cookie = `tcm_token=${token.access_token};expires=${date.toUTCString()};path=/;SameSite=Lax`;
 
       // Transition away login element
-      $('#login').transition({animation: 'fade', duration: 800})
+      setTimeout(() => $('#login').transition({animation: 'fade', duration: 800}), 800);
 
       // Redirect to indicated page
       setTimeout(() => {
+        // Get redirect URL from the search params
         const redirectURL = new URLSearchParams(window.location.search).get('redirect');
-        if (redirectURL) {
-          window.location.href = redirectURL;
-        } else {
-          window.location.href = '/';
-        }
-      }, 1000);
+        if (redirectURL) { window.location.href = redirectURL; }
+        else {             window.location.href = '/';         }
+      }, 1600);
     }, error: response => {
       $.toast({
         class: 'error',
         title: 'Authentication Error',
         message: response.responseJSON.detail
       });
-      makeRedder(45);
+      makeRedder(20);
     },
   });
 }
@@ -85,4 +74,5 @@ $(document).ready(function() {
   $('#password').on('keydown', event => {
     if (event.keyCode === 13 || event.key === "Enter") { submitForm(); }
   });
+  $('svg').toggleClass('active', true);
 });
