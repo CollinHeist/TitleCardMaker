262c0e8a7e438995301b1ec540e6142ea3d2a3c9
Collin Heist <collinheist@gmail.com>
2024-01-26 20:57:58 -0700

    [74] Adjust 3122c0553b1e schema revision
    - Add downgrade for Font replacements
    - Add migrations for old style cardinal/ordinal and titlecase ETF and season title ranges

--- app/alembic/versions/3122c0553b1e_separate_font_replacements.py
+++ app/alembic/versions/3122c0553b1e_separate_font_replacements.py
@@ -1,7 +1,9 @@
-"""Separate Font replacements
+"""Separate Font replacements; migrate cardinal and ordinal formatting
 Modify Font table
 - Remove replacements column
 - Create new replacements_in and replacements_out columns
+Modify Series and Template tables
+- Convert "old" style cardinal and ordinal specifications to new style
 
 Revision ID: 3122c0553b1e
 Revises: b99ce3bfdfbd
@@ -30,13 +32,46 @@ Base = declarative_base()
 class Font(Base):
     __tablename__ = 'font'
 
-    # Referencial arguments
     id = sa.Column(sa.Integer, primary_key=True)
     # Old Column for migration
     replacements = sa.Column(sa.JSON)
     # New columns
     replacements_in = sa.Column(sa.JSON)
     replacements_out = sa.Column(sa.JSON)
+
+class Series(Base):
+    __tablename__ = 'series'
+
+    id = sa.Column(sa.Integer, primary_key=True)
+    episode_text_format = sa.Column(sa.String)
+    season_titles = sa.Column(sa.JSON)
+
+class Template(Base):
+    __tablename__ = 'template'
+
+    id = sa.Column(sa.Integer, primary_key=True)
+    episode_text_format = sa.Column(sa.String)
+    season_titles = sa.Column(sa.JSON)
+
+_conversions = {
+    '{season_number_cardinal}': '{to_cardinal(season_number)}',
+    '{season_number_ordinal}': '{to_ordinal(season_number)}',
+    '{season_number_cardinal_title}': '{titlecase(to_cardinal(season_number))}',
+    '{season_number_ordinal_title}': '{titlecase(to_ordinal(season_number))}',
+    '{episode_number_cardinal}': '{to_cardinal(episode_number)}',
+    '{episode_number_ordinal}': '{to_ordinal(episode_number)}',
+    '{episode_number_cardinal_title}': '{titlecase(to_cardinal(episode_number))}',
+    '{episode_number_ordinal_title}': '{titlecase(to_ordinal(episode_number))}',
+    '{absolute_number_cardinal}': '{to_cardinal(absolute_number)}',
+    '{absolute_number_ordinal}': '{to_ordinal(absolute_number)}',
+    '{absolute_number_cardinal_title}': '{titlecase(to_cardinal(absolute_number))}',
+    '{absolute_number_ordinal_title}': '{titlecase(to_ordinal(absolute_number))}',
+    '{absolute_episode_number_cardinal}': '{to_cardinal(absolute_episode_number)}',
+    '{absolute_episode_number_ordinal}': '{to_ordinal(absolute_episode_number)}',
+    '{absolute_episode_number_cardinal_title}': '{titlecase(to_cardinal(absolute_episode_number))}',
+    '{absolute_episode_number_ordinal_title}': '{titlecase(to_ordinal(absolute_episode_number))}',
+}
+
 
 def upgrade() -> None:
     log = contextualize(logger)
@@ -64,6 +99,24 @@ def upgrade() -> None:
             log.debug(f'Migrated Font[{font.id}].replacements_in = {font.replacements_in}')
             log.debug(f'Migrated Font[{font.id}].replacements_out = {font.replacements_out}')
 
+    # Modify Series+Template cardinal and ordinal values
+    for obj in session.query(Series).all() + session.query(Template).all():
+        if isinstance((_old := obj.episode_text_format), str):
+            for rin, rout in _conversions.items():
+                obj.episode_text_format = obj.episode_text_format.replace(
+                    rin, rout,
+                )
+            if _old != obj.episode_text_format:
+                log.debug(f'Migrated {obj.__tablename__}[{obj.id}]'
+                          f'.episode_text_format = {obj.episode_text_format}')
+        if isinstance(obj.season_titles, dict):
+            for k, v in obj.season_titles.items():
+                for rin, rout in _conversions.items():
+                    obj.season_titles[k] = obj.season_titles[k].replace(rin, rout)
+                if v != obj.season_titles[k]:
+                    log.debug(f'Migrated {obj.__tablename__}[{obj.id}]'
+                              f'.season_titles = {obj.season_titles}')
+
     # Commit changes
     session.commit()
 
@@ -77,10 +130,24 @@ def upgrade() -> None:
 def downgrade() -> None:
     log = contextualize(logger)
     log.debug(f'Downgrading SQL Schema to Version[{down_revision}]..')
-    log.critical(f'SQL schema is not backwards compatible, Font replacements will be reset')
 
     with op.batch_alter_table('font', schema=None) as batch_op:
-        batch_op.add_column(sa.Column('replacements', sqlite.JSON(), nullable=True))
+        batch_op.add_column(sa.Column(
+            'replacements', sqlite.JSON(), nullable=True
+        ))
+
+    # Perform data un-migration
+    session = Session(bind=op.get_bind())
+
+    for font in session.query(Font).all():
+        font.replacements = dict(zip(font.replacements_in, font.replacements_out))
+        if font.replacements:
+            log.debug(f'Un-migrated Font[{font.id}].replacements = {font.replacements}')
+
+    # Commit changes
+    session.commit()
+
+    with op.batch_alter_table('font', schema=None) as batch_op:
         batch_op.drop_column('replacements_out')
         batch_op.drop_column('replacements_in')
 
