9bcad8197d09d1b6bbefb237d19318ec92a8d06a
Collin Heist <collinheist@gmail.com>
2024-03-25 18:02:19 -0600

    [83] Modify import page
    - Remove YAML preference file importing (not necessary, faster to just set directly)
    - Modify card importer to use new import API endpoints

--- app/templates/js/import.js
+++ app/templates/js/import.js
@@ -21,52 +21,54 @@ async function initSeriesImport() {
     placeholder: 'Select 1+ Series',
     useLabels: false,
     values: [
-      // All series toggle
-      {name: 'Select All Series', actionable: true},
-      {name: 'Series', type: 'header'},
-      // All series
-      ...allSeries.sort((a, b) => a.name.localeCompare(b.name)).map(series => {
-        return {
-          name: series.name,
-          value: series.id,
-          text: series.name,
-          description: series.year,
-          sortSelect: true,
-          descriptionVertical: true,
-        };
-      }),
-    ], onChange: (value, text, $selectedItem) => {
-      // Disable directory field if multiple series are selected
-      if (value.includes(',')) {
-        $('.field[data-value="directory"]').toggleClass('disabled', true);
-      // Single series selected, enable field and update with series directory
-      } else {
-        $('.field[data-value="directory"]').toggleClass('disabled', false);
-        if (seriesMap[value] === undefined || seriesMap[value].directory === null) {
-          $('input[name="directory"]')[0].value = '';
-        } else {
-          $('input[name="directory"]')[0].value = seriesMap[value].directory;
-        }
-      }
-    }, onActionable: (value, text, $selected) => {
-      $('.dropdown[data-value="series_id"]').dropdown({
-        ignoreDiacritics: true,
-        sortSelect: true,
-        placeholder: 'Select 1+ Series',
-        useLabels: false,
-        values: allSeries.sort((a, b) => a.name.localeCompare(b.name)).map(series => {
+        // All series toggle
+        {name: 'Select All Series', actionable: true},
+        {name: 'Series', type: 'header'},
+        // All series
+        ...allSeries.sort((a, b) => a.name.localeCompare(b.name)).map(series => {
           return {
             name: series.name,
             value: series.id,
-            selected: true,
             text: series.name,
             description: series.year,
             sortSelect: true,
             descriptionVertical: true,
           };
         }),
-      });
-    },
+      ],
+    onChange: (value, text, $selectedItem) => {
+        // Disable directory field if multiple series are selected
+        if (value.includes(',')) {
+          $('.field[data-value="directory"]').toggleClass('disabled', true);
+        // Single series selected, enable field and update with series directory
+        } else {
+          $('.field[data-value="directory"]').toggleClass('disabled', false);
+          if (seriesMap[value] === undefined || seriesMap[value].directory === null) {
+            $('input[name="directory"]')[0].value = '';
+          } else {
+            $('input[name="directory"]')[0].value = seriesMap[value].directory;
+          }
+        }
+      },
+    onActionable: (value, text, $selected) => {
+        $('.dropdown[data-value="series_id"]').dropdown({
+          ignoreDiacritics: true,
+          sortSelect: true,
+          placeholder: 'Select 1+ Series',
+          useLabels: false,
+          values: allSeries.sort((a, b) => a.name.localeCompare(b.name)).map(series => {
+            return {
+              name: series.name,
+              value: series.id,
+              selected: true,
+              text: series.name,
+              description: series.year,
+              sortSelect: true,
+              descriptionVertical: true,
+            };
+          }),
+        });
+      },
   });
   $('#import-cards-form .dropdown[data-value="series_id"]').toggleClass('loading', false);
 }
@@ -115,43 +117,12 @@ async function initAll() {
     importSeries();
   });
 
-  // Preferences form submission
-  $('#preferences-form').on('submit', event => {
-    // Prevent page reload
-    event.preventDefault();
-    const form = new FormData(event.target);
-    // Determine which queries to submit
-    if ($('#preferences-form .checkbox[data-value="options"]').checkbox('is checked')) {
-      submitYamlForm({
-        url: '/api/import/preferences/options',
-        data: Object.fromEntries(form),
-        formId: '#preferences-form',
-        name: 'Options',
-      });
-    }
-    if ($('#preferences-form .checkbox[data-value="connections"]').checkbox('is checked')) {
-      submitYamlForm({
-        url: '/api/import/preferences/connection/all',
-        data: Object.fromEntries(form),
-        formId: '#preferences-form',
-        name: 'Connections',
-      });
-    }
-    if ($('#preferences-form .checkbox[data-value="syncs"]').checkbox('is checked')) {
-      submitYamlForm({
-        url: '/api/import/preferences/sync',
-        data: Object.fromEntries(form),
-        formId: '#preferences-form',
-        name: 'Syncs',
-      });
-    }
-  });
-
   // Series YAML form submission
   $('#series-form').on('submit', event => {
     // Prevent page reload
     event.preventDefault();
     const form = new FormData(event.target);
+
     // Turn off error status of the form
     $('#series-form').toggleClass('error', false);
 
@@ -161,6 +132,7 @@ async function initAll() {
 
 /**
  * Submit an API request to load the Cards for the Series with the given ID.
+ * @param {number} seriesId - ID of the Series whose Cards are being loaded.
  */
 function loadCards(seriesId) {
   // Get associated Media Server to load cards into, default Plex
@@ -172,6 +144,27 @@ function loadCards(seriesId) {
   });
 }
 
+/**
+ * Convert the given `data` object into a `FormData` object. Modifies `formData`
+ * in-place. Stolen from https://stackoverflow.com/a/42483509/8706916.
+ * @param {FormData} formData - Form being populated.
+ * @param {Object} data - Arbitrary data used to populate `formData`.
+ * @param {?string} parentKey - Parent key to store the data in, used for
+ * recursive object conversion.
+ */
+function _buildFormData(formData, data, parentKey) {
+  if (data && typeof data === 'object' && !(data instanceof Date)
+      && !(data instanceof File) && !(data instanceof Blob)) {
+    Object.keys(data).forEach(key => {
+      _buildFormData(formData, data[key], parentKey ? `${parentKey}[${key}]` : key);
+    });
+  } else {
+    const value = data == null ? '' : data;
+
+    formData.append(parentKey, value);
+  }
+}
+
 function importSeries() {
   // Get associated Series
   let seriesIds = $('input[name="series_id"]')[0].value;
@@ -180,30 +173,36 @@ function importSeries() {
     return;
   }
   $('.dropdown[data-value="series_id"]').toggleClass('error', false);
-  seriesIds = seriesIds.split(',');
+  seriesIds = seriesIds.split(',').map(id => parseInt(id));
+
   // Data for request
   const directoryValue = $('input[name="directory"]')[0].value
-  const data = {
+  let rawData = {
     series_ids: seriesIds,
     directory: directoryValue === '' ? null : directoryValue,
     image_extension: $('input[name="image_extension"]')[0].value,
     force_reload: $('.checkbox[data-value="force_reload"]').checkbox('is unchecked'),
   };
-  // URL for request if submitting >1 series
-  const url = seriesIds.length > 1 ? '/api/import/series/cards' : `/api/import/series/${seriesIds[0]}/cards`;
+
   // Submit request
   $('.segment .dimmer').toggleClass('active', true);
+  const url = seriesIds.length > 1
+    ? '/api/import/series/cards'
+    : `/api/import/series/${seriesIds[0]}/cards/directory`
+  ;
+
   $.ajax({
     type: 'POST',
-    url: url,
-    data: JSON.stringify(data),
+    url,
+    data: JSON.stringify(rawData),
     contentType: 'application/json',
     success: () => {
       showInfoToast('Imported Title Cards');
       if ($('.checkbox[data-value="load_afterwards"]').checkbox('is checked')) {
         for (let series_id of seriesIds) { loadCards(series_id); }
       }
-    }, error: response => showErrorToast({title: 'Error importing Cards', response}),
+    },
+    error: response => showErrorToast({title: 'Error importing Cards', response}),
     complete: () => $('.segment .dimmer').toggleClass('active', false),
   });
 }
