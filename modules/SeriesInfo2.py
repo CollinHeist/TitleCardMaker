57c9c4d8edce69e1d18533621c053f9b488c0db5
Collin Heist <collinheist@gmail.com>
2024-04-15 17:32:31 -0600

    [60] Update SeriesInfo2.py

--- modules/SeriesInfo2.py
+++ modules/SeriesInfo2.py
@@ -87,12 +87,12 @@ class SeriesInfo(DatabaseInfoContainer):
         self.name = name
         self.year = year
         self.emby_id = InterfaceID(emby_id, type_=str, libraries=True)
-        self.imdb_id = None
+        self.imdb_id: Optional[str] = None
         self.jellyfin_id = InterfaceID(jellyfin_id, type_=str, libraries=True)
         self.sonarr_id = InterfaceID(sonarr_id, type_=int, libraries=False)
-        self.tmdb_id = None
-        self.tvdb_id = None
-        self.tvrage_id = None
+        self.tmdb_id: Optional[int] = None
+        self.tvdb_id: Optional[int] = None
+        self.tvrage_id: Optional[int] = None
         self.match_titles = match_titles
 
         self.set_imdb_id(imdb_id)
