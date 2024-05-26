47a706abe2cc9bfc3d4d035505177eb360a53410
Collin Heist <collinheist@gmail.com>
2024-05-26 11:59:57 -0600

    [31] Use Formula 1 card type for "f1" identifier

--- modules/TitleCard.py
+++ modules/TitleCard.py
@@ -85,6 +85,7 @@ class TitleCard:
         'comic book': ComicBookTitleCard,
         'cutout': CutoutTitleCard,
         'divider': DividerTitleCard,
+        'f1': FormulaOneTitleCard,
         'fade': FadeTitleCard,
         'formula 1': FormulaOneTitleCard,
         'frame': FrameTitleCard,
