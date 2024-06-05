7ae7ae4c0f7e8976a190d0c11fb7a4064c67e94d
Collin Heist <collinheist@gmail.com>
2024-06-05 13:13:05 -0600

    [73] Provide default WebInterface.get params

--- modules/WebInterface.py
+++ modules/WebInterface.py
@@ -138,7 +138,12 @@ class WebInterface:
         return None
 
 
-    def get(self, url: str, params: dict, *, cache: bool = True) -> Any:
+    def get(self,
+            url: str,
+            params: dict = {},
+            *,
+            cache: bool = True,
+        ) -> Union[dict, Any]:
         """
         Wrapper for getting the JSON return of the specified GET
         request. If the provided URL and parameters are identical to the
