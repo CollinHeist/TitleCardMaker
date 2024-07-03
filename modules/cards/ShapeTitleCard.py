496eea1a59fb34f0c284412e9bfe00de033e31d5
Collin Heist <collinheist@gmail.com>
2024-07-03 12:30:44 -0600

    [14] Scale side length by +30% per line, not +25%

--- modules/cards/ShapeTitleCard.py
+++ modules/cards/ShapeTitleCard.py
@@ -174,10 +174,8 @@ class ShapeTitleCard(BaseCardType):
         self.stroke_color = stroke_color
         self.text_position: TextPosition = text_position
 
-        # Scale side length by for multiline titles
-        self.__line_count = len(title_text.splitlines())
-        if self.__line_count > 1:
-            self.length *= 1.0 + (0.25 * (self.__line_count - 1))
+        # Scale side length by 30% for each additional line in the title
+        self.length *= 1.0 + (0.30 * (len(title_text.splitlines()) - 1))
 
         # Implementation variables
         self.__title_width = None
