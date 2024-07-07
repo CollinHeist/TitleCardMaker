9e4e5eec6cf377fef65f68242d534b8d9c8c92c4
Collin Heist <collinheist@gmail.com>
2024-07-07 14:56:53 -0600

    [99] Add mocha reporting node commands

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,14 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   projectId: "sod537",
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    charts: true,
+    overwrite: false,
+    html: false,
+    json: true,
+  },
   e2e: {
     baseUrl: 'http://localhost:4242',
   },
