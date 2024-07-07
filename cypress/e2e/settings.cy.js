2b6ec81a3775a4e0b60cb127b368c6eaabb178ab
Collin Heist <collinheist@gmail.com>
2024-07-07 13:49:17 -0600

    [94] Create outline of FE Settings tests
    None are implemented yet

---
+++ cypress/e2e/settings.cy.js
@@ -0,0 +1,68 @@
+describe('Settings', () => {
+  beforeEach(() => cy.visit('/settings'));
+
+  it('Changes the root folder settings', () => {
+
+  });
+  
+  it('Changes the series and episode data settings', () => {
+
+  });
+  
+  it('Selects an episode data source, deletes it, and then verifies the deletion', () => {
+
+  });
+
+  it('Selects an image source, deletes it, and verifies the deletion', () => {
+
+  });
+
+  it('Changes the default card type preview', () => {
+
+  });
+
+  it('Changes the title card settings', () => {
+
+  });
+
+  it('Enters invalid card dimensions', () => {
+
+  });
+
+  it('Selects a default template, deletes it, and verifies the deletion', () => {
+
+  });
+
+  it('Selects global extras', () => {
+
+  });
+
+  it('Changes the file naming settings', () => {
+
+  });
+
+  it('Enters an invalid title card filename format and the change is rejected', () => {
+
+  });
+
+  it('Enters an invalid folder format and the change is rejected', () => {
+
+  });
+
+  it('Changes the web interface settings', () => {
+
+  });
+
+  it('Enters an invalid home page size', () => {
+
+  });
+
+  it('Enters an invalid episode data table page size', () => {
+
+  });
+
+  it('Sees the current version', () => {
+
+  });
+
+});
